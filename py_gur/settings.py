b0e3bb77ace30d754b4ccefacd1782b7028326a6
brandoshmando <brancraft@gmail.com>
2014-11-13 13:13:25 -0700

    Added and restructured packages within settings.py

--- py_gur/settings.py
+++ py_gur/settings.py
@@ -29,13 +29,23 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = (
+INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS +LOCAL_APPS
+
+THIRD_PARTY_APPS = (
+    'ipython',
+    )
+
+DEFAULT_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+)
+
+LOCAL_APPS = (
+    'image_share'
 )
 
 MIDDLEWARE_CLASSES = (
