f19d7ce0e32a1b6ddd0324bad4120b33c5a05145
brandoshmando <brancraft@gmail.com>
2014-11-14 12:02:02 -0700

    Add model defs for user and image

--- img_share/models.py
+++ img_share/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
+class User(models.Model):
+  first_name = models.CharField(max_length=25)
+  last_name = models.CharField(max_length=25)
+  email = models.CharField(max_length=60)
+
 class Image(models.Model):
-  pass
+  title = models.CharField(max_length=40)
+  artist_name = models.CharField(max_length=60)
+  image_link = models.CharField()
+  user = models.ForeignKey(User)
